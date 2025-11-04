e727daf3509d368a806a07123fe6183d5a010c23
aymensellaouti <aymen.sellaouti@gmail.com>
2025-11-04 11:25:45 +0100

    exemple rxjs

--- src/app/rxjs/test-observable/test-observable.component.ts
+++ src/app/rxjs/test-observable/test-observable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from "@angular/core";
+import { Component, inject, OnDestroy } from "@angular/core";
 import { Observable, Subscription, filter, map } from "rxjs";
 import { ToastrService } from "ngx-toastr";
 
@@ -9,7 +9,8 @@ import { ToastrService } from "ngx-toastr";
 })
 export class TestObservableComponent {
   firstObservable$: Observable<number>;
-
+  toastr = inject(ToastrService);
+  //counter = 5;
   constructor(private toaster: ToastrService) {
     this.firstObservable$ = new Observable((observer) => {
       let i = 5;
@@ -22,5 +23,24 @@ export class TestObservableComponent {
         }
       }, 1000);
     });
+
+    // Fama chkoun interested bech i9ayed
+    this.firstObservable$.subscribe({
+      next: (dataJDida) => console.log(dataJDida * 3),
+    }); // Fama chkoun interested bech i9ayed
+    // this.firstObservable$.subscribe({
+    //   next: (dataJDida) => (this.counter = dataJDida),
+    // });
+    // Fama chkoun akher interested bech i9ayed
+
+    setTimeout(() => {
+      this.firstObservable$.subscribe({
+        next: (value) => {
+          value = value * 2;
+          this.toaster.info("" + value);
+        },
+        complete: () => this.toaster.error("BOOOOM !!!!!"),
+      });
+    }, 3000);
   }
 }
